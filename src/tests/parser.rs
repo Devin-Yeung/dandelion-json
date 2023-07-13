431601fadadaa3c0618afd88e3673a3231f170a8
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-13 14:48:13 +0800

    Refactor: make tests easier to debug

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,44 +1,46 @@
 use crate::data::{Value, ValueType};
+use crate::errors::Errors;
 use crate::parser::Parser;
+use std::fmt::Debug;
+
+fn parse_assert<S, E>(json: S, expect: &E)
+where
+    S: AsRef<str>,
+    E: Eq + Debug,
+    std::result::Result<Value, Errors>: PartialEq<E>,
+{
+    assert_eq!(&Parser::parse(&json), expect)
+}
 
 #[test]
 fn parse_null() {
-    let data = vec!["null", " null", "\t null", "\n null"];
-
-    data.into_iter().for_each(|json| {
-        assert_eq!(
-            Parser::parse(json),
-            Ok(Value {
-                v_type: ValueType::Null,
-            })
-        )
-    })
+    let null = Ok(Value {
+        v_type: ValueType::Null,
+    });
+    parse_assert("null", &null);
+    parse_assert(" null", &null);
+    parse_assert("\n null", &null);
+    parse_assert("\t null", &null);
 }
 
 #[test]
 fn parse_true() {
-    let data = vec!["true", " true", "\t true", "\n true"];
-
-    data.into_iter().for_each(|json| {
-        assert_eq!(
-            Parser::parse(json),
-            Ok(Value {
-                v_type: ValueType::Bool(true),
-            })
-        )
-    })
+    let true_ = Ok(Value {
+        v_type: ValueType::Bool(true),
+    });
+    parse_assert("true", &true_);
+    parse_assert(" true", &true_);
+    parse_assert("\n true", &true_);
+    parse_assert("\t true", &true_);
 }
 
 #[test]
 fn parse_false() {
-    let data = vec!["false", " false", "\t false", "\n false"];
-
-    data.into_iter().for_each(|json| {
-        assert_eq!(
-            Parser::parse(json),
-            Ok(Value {
-                v_type: ValueType::Bool(false),
-            })
-        )
-    })
+    let false_ = Ok(Value {
+        v_type: ValueType::Bool(false),
+    });
+    parse_assert("false", &false_);
+    parse_assert(" false", &false_);
+    parse_assert("\n false", &false_);
+    parse_assert("\t false", &false_);
 }
