286dd97e06528de86a87eed40ec726e04b086e09
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-14 01:13:22 +0800

    Feat: Number Parser (ECMA-404)

--- src/parser.rs
+++ src/parser.rs
@@ -10,6 +10,38 @@ pub struct Parser<'json> {
     context: Context<'json>,
 }
 
+pub struct Iter<'json> {
+    partial_json: &'json str,
+    cursor: usize,
+}
+
+impl Iter<'_> {
+    pub fn cursor(&self) -> usize {
+        self.cursor
+    }
+}
+
+impl Iterator for Iter<'_> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = self.partial_json.chars().nth(self.cursor);
+        self.cursor += 1;
+        return ret;
+    }
+}
+
+impl Iter<'_> {
+    pub fn peek(&mut self) -> Option<char> {
+        self.partial_json.chars().nth(self.cursor)
+    }
+
+    /// str which have been looked by this iter
+    pub fn looked(&mut self) -> &str {
+        &self.partial_json[..self.cursor]
+    }
+}
+
 impl<'json> Context<'json> {
     pub fn new<S>(json: &S) -> Context
     where
@@ -17,6 +49,13 @@ impl<'json> Context<'json> {
     {
         Context {
             json: json.as_ref(),
+            cursor: 0,
+        }
+    }
+
+    pub fn iter(&self) -> Iter {
+        Iter {
+            partial_json: &self.json[self.cursor..],
             cursor: 0,
         }
     }
@@ -82,6 +121,114 @@ impl Parser<'_> {
         };
     }
 
+    fn consume_digits(iter: &mut Iter, min: usize) -> Result<()> {
+        let mut cnt: usize = 0;
+        /* consume at least n digits */
+        while let Some(c) = iter.peek() {
+            if c.is_ascii_digit() {
+                iter.next();
+                cnt += 1;
+            } else {
+                if cnt < min {
+                    return Err(Errors::InvalidValue);
+                }
+                break;
+            }
+        }
+
+        return if cnt >= min {
+            Ok(())
+        } else {
+            Err(Errors::InvalidValue)
+        };
+    }
+
+    fn parse_number(&mut self) -> Result<Value> {
+        let mut iter = self.context.iter();
+
+        /* match minus sign */
+        if iter.peek() == Some('-') {
+            iter.next();
+        }
+
+        /* match the digits before decimal point */
+        match iter.next() {
+            Some('0') => { /* continue */ }
+            Some(c) => match c.is_ascii_digit() {
+                true => Parser::consume_digits(&mut iter, 0)?,
+                false => return Err(Errors::InvalidValue),
+            },
+            None => return Err(Errors::InvalidValue),
+        }
+
+        /* match decimal point */
+        match iter.peek() {
+            None => { /* no fraction part, continue */ }
+            Some(c) => {
+                match c {
+                    '.' => {
+                        iter.next(); /* skip the decimal point */
+                        Parser::consume_digits(&mut iter, 1)?
+                    }
+                    'e' | 'E' => { /* exp field, continue */ }
+                    _ => {
+                        return Err(Errors::InvalidValue);
+                    }
+                }
+            }
+        }
+
+        /* match exponential field */
+        match iter.peek() {
+            None => { /* no exp field, continue */ }
+            Some(c) => {
+                /* match e | E */
+                match c {
+                    'e' | 'E' => {
+                        iter.next(); /* continue parsing */
+                    }
+                    _ => {
+                        return Err(Errors::InvalidValue);
+                    }
+                }
+
+                /* match + | - */
+                match iter.peek() {
+                    None => {
+                        return Err(Errors::InvalidValue);
+                    }
+                    Some(c) => {
+                        match c {
+                            '+' | '-' => {
+                                iter.next();
+                            }
+                            /* + | - is optional, if not appear, can only follow digits */
+                            _ => {
+                                if !c.is_ascii_digit() {
+                                    return Err(Errors::InvalidValue);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                /* match one or more digits */
+                Parser::consume_digits(&mut iter, 1)?;
+            }
+        }
+        // F**king Painful! I will definitely use regex in the future :)
+        // Regex Ver: r"(?:^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?)"
+
+        let num = iter.looked();
+        let steps = num.len();
+        let num = num.parse::<f64>().map_err(|_| Errors::InvalidValue)?;
+        self.context.advance_n(steps);
+
+        Ok(Value {
+            v_type: ValueType::Number(num),
+        })
+    }
+
     fn parse_value(&mut self) -> Result<Value> {
         return match self.context.cur() {
             None => Err(Errors::ReachEOF),
@@ -89,7 +236,7 @@ impl Parser<'_> {
                 't' => self.parse_literal("true", ValueType::Bool(true)),
                 'f' => self.parse_literal("false", ValueType::Bool(false)),
                 'n' => self.parse_literal("null", ValueType::Null),
-                _ => Err(Errors::InvalidValue),
+                _ => self.parse_number(),
             },
         };
     }
--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -8,6 +8,13 @@ macro_rules! json_assert {
     }};
 }
 
+macro_rules! invalid_assert {
+    ($json:expr, $error:expr) => {{
+        use $crate::errors::Errors::*;
+        assert_eq!($crate::parser::Parser::parse(&$json), Err($error))
+    }};
+}
+
 #[test]
 fn parse_null() {
     json_assert!("null", Null);
@@ -33,4 +40,57 @@ fn parse_false() {
     json_assert!(" false", Bool(false));
     json_assert!("\n false", Bool(false));
     json_assert!("\t false", Bool(false));
+}
+
+#[test]
+fn simple_number() {
+    json_assert!("0", Number(0.0));
+    json_assert!("-0", Number(0.0));
+    json_assert!("-0.0", Number(0.0));
+    json_assert!("1", Number(1.0));
+    json_assert!("-1", Number(-1.0));
+    json_assert!("1.5", Number(1.5));
+    json_assert!("-1.5", Number(-1.5));
+    json_assert!("3.1416", Number(3.1416));
+}
+
+#[test]
+fn parse_num_with_exp() {
+    json_assert!("1E10", Number(1E10));
+    json_assert!("1e10", Number(1e10));
+    json_assert!("1E+10", Number(1E+10));
+    json_assert!("1E-10", Number(1E-10));
+    json_assert!("-1E10", Number(-1E10));
+    json_assert!("-1e10", Number(-1e10));
+    json_assert!("-1E+10", Number(-1E+10));
+    json_assert!("-1E-10", Number(-1E-10));
+    json_assert!("1.234E+10", Number(1.234E+10));
+    json_assert!("1.234E-10", Number(1.234E-10));
+}
+
+#[test]
+fn parse_num_edge_case() {
+    json_assert!("1e-10000", Number(0.0)); /* must underflow */
+    json_assert!("1.0000000000000002", Number(1.0000000000000002)); /* the smallest number > 1 */
+    json_assert!("4.9406564584124654e-324", Number(4.9406564584124654e-324)); /* minimum denormal */
+    json_assert!("-4.9406564584124654e-324", Number(-4.9406564584124654e-324));
+    json_assert!("2.2250738585072009e-308", Number(2.2250738585072009e-308)); /* Max subnormal double */
+    json_assert!("-2.2250738585072009e-308", Number(-2.2250738585072009e-308));
+    json_assert!("2.2250738585072014e-308", Number(2.2250738585072014e-308)); /* Min normal positive double */
+    json_assert!("-2.2250738585072014e-308", Number(-2.2250738585072014e-308));
+    json_assert!("1.7976931348623157e+308", Number(1.7976931348623157e+308)); /* Max double */
+    json_assert!("-1.7976931348623157e+308", Number(-1.7976931348623157e+308));
+}
+
+#[test]
+fn parse_invalid_num() {
+    /* invalid number */
+    invalid_assert!("+0", InvalidValue);
+    invalid_assert!("+1", InvalidValue);
+    invalid_assert!(".123", InvalidValue); /* at least one digit before '.' */
+    invalid_assert!("1.", InvalidValue); /* at least one digit after '.' */
+    invalid_assert!("INF", InvalidValue);
+    invalid_assert!("inf", InvalidValue);
+    invalid_assert!("NAN", InvalidValue);
+    invalid_assert!("nan", InvalidValue);
 }
