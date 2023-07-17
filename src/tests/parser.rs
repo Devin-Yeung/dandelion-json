f1df495c8c9033ab6a8e96b3761049123f0f6ca2
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-17 16:29:03 +0800

    Feat: more array tests

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -168,4 +168,25 @@ fn valid_array() {
     json_assert!("[ true ]", arr![Bool(true)]);
     json_assert!("[null,null]", arr!(Null, Null));
     json_assert!("[]", arr!());
+    json_assert!("[123]", arr!(Number(123.0)));
+    json_assert!("[123 ]", arr!(Number(123.0)));
+    json_assert!(
+        r#"[ null , false , true , 123 , "abc" ]"#,
+        arr!(
+            Null,
+            Bool(false),
+            Bool(true),
+            Number(123.0),
+            String("abc".to_string())
+        )
+    );
+    json_assert!(
+        "[ [ ] , [ 0 ] , [ 0 , 1 ] , [ 0 , 1 , 2 ] ]",
+        arr!(
+            arr!(),
+            arr!(Number(0.0)),
+            arr!(Number(0.0), Number(1.0)),
+            arr!(Number(0.0), Number(1.0), Number(2.0)),
+        )
+    )
 }
