54bfaad8a0c1ddc64359a01700fc24fe5957b916
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-19 19:25:41 +0800

    Feat: test parsing valid json object

--- src/tests/parser/object.rs
+++ src/tests/parser/object.rs
@@ -1,4 +1,45 @@
-use crate::invalid_assert;
+use crate::data::Value;
+use crate::data::Value::Number;
+use crate::{arr, invalid_assert, json_assert, str};
+use std::collections::HashMap;
+
+#[test]
+fn valid_object() {
+    let json = r#"
+    {
+        "n" : null ,
+        "f" : false ,
+        "t" : true ,
+        "i" : 123 ,
+        "s" : "abc",
+        "a" : [ 1, 2, 3 ],
+        "o" : { "1" : 1, "2" : 2, "3" : 3 }
+    }
+    "#;
+    let expected = Value::Object({
+        let mut map = HashMap::<String, Value>::new();
+        map.insert(String::from("n"), Value::Null);
+        map.insert(String::from("f"), Value::Bool(false));
+        map.insert(String::from("t"), Value::Bool(true));
+        map.insert(String::from("i"), Number(123.0));
+        map.insert(String::from("s"), str!("abc"));
+        map.insert(
+            String::from("a"),
+            arr![Number(1.0), Number(2.0), Number(3.0)],
+        );
+        map.insert(String::from("o"), {
+            let mut map = HashMap::<String, Value>::new();
+            map.insert(String::from("1"), Number(1.0));
+            map.insert(String::from("2"), Number(2.0));
+            map.insert(String::from("3"), Number(3.0));
+            Value::Object(map)
+        });
+        map
+    });
+    json_assert!(json, expected);
+    // maybe I should write a macro to deal with this annoying stuff
+    // see [dtolnay's solution](https://github.com/serde-rs/json/blob/master/src/macros.rs)
+}
 
 #[test]
 fn key_is_missing() {
