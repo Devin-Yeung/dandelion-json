86cea6dc0eab4851517d53806a4a72df5558093a
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-07-21 19:39:10 +0800

    Feat: `get` and `get_mut` for `Value`

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,5 @@
+use crate::index::Index;
+
 #[derive(Debug, PartialEq)]
 pub enum Value {
     Null,
@@ -6,4 +8,33 @@ pub enum Value {
     String(String),
     Array(Vec<Value>),
     Object(std::collections::HashMap<String, Value>),
+}
+
+/// some docs are grabbed from dtolnay's [serde_json], thank you!
+///
+/// [serde_json]: https://github.com/serde-rs/json/blob/master/src/value/mod.rs
+impl Value {
+    /// Index into a JSON array or map. A string index can be used to access a
+    /// value in a map, and a usize index can be used to access an element of an
+    /// array.
+    ///
+    /// Returns `None` if the type of `self` does not match the type of the
+    /// index, for example if the index is a string and `self` is an array or a
+    /// number. Also returns `None` if the given key does not exist in the map
+    /// or the given index is not within the bounds of the array.
+    pub fn get<I: Index>(&self, index: I) -> Option<&Value> {
+        index.index_into(self)
+    }
+
+    /// Mutably index into a JSON array or map. A string index can be used to
+    /// access a value in a map, and a usize index can be used to access an
+    /// element of an array.
+    ///
+    /// Returns `None` if the type of `self` does not match the type of the
+    /// index, for example if the index is a string and `self` is an array or a
+    /// number. Also returns `None` if the given key does not exist in the map
+    /// or the given index is not within the bounds of the array.
+    pub fn get_mut<I: Index>(&mut self, index: I) -> Option<&mut Value> {
+        index.index_into_mut(self)
+    }
 }
